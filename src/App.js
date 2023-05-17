dc33e20b9f6dea976a7fa8ff5ceaf33945caab13
ADIL MUHAMMED Y <adilmuhammedy@gmail.com>
2023-05-17 17:43:52 +0530

    new

--- src/App.js
+++ src/App.js
@@ -1,9 +1,16 @@
-import React from 'react';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import Home from './Homepage';
+import BookTickets from './bookticket';
 
 function App() {
   return (
-    <Home/>
+    <Router>
+      <Switch>
+        <Route exact path="/" component={Home} />
+        <Route path="/book-tickets" component={BookTickets} />
+      </Switch>
+    </Router>
   );
 }
-  export default App;
+
+export default App;
--- src/bookticket.js
+++ src/bookticket.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import './bookticket';
+import './bookticket.css';
 
 function BookTicket() {
     return (
@@ -12,8 +12,50 @@ function BookTicket() {
           <h4 className="login">LOGIN</h4>
         </header>
         <div className="rectangle">
-            <h2 className="booktickets">BOOK TICKETS</h2>
+     
         </div>
+        <h2 className="book1">BOOK TICKETS</h2>
+        <div className="bookfrom">
+          <h4 className="from">FROM</h4>
+          <div className='selectfrom'>
+          <select value="from" className="selectfrom">
+          <option value="SELECT">----SELECT----</option>
+          <option value="1">KAKKANAD</option>
+          <option value="2">VYTTILA</option>
+          <option value="3">HIGHCOURT</option>
+          <option value="4">VYPIN</option>
+          </select>
+          </div>
+          <h4 className="to">TO</h4>
+          <div className='selectto'>
+          <select value="to" className="selectto">
+          <option value="SELECT">----SELECT----</option>
+          <option value="1">KAKKANAD</option>
+          <option value="2">VYTTILA</option>
+          <option value="3">HIGHCOURT</option>
+          <option value="4">VYPIN</option>
+          </select>
+          </div>
+          <h4 className="tickettype">Ticket-type</h4>
+          <div className='type'>
+          <select value="Ticket-type" className="type">
+          <option value="">One-way</option>
+          <option value="option1">Two-way</option>
+          </select>
+          </div>
+          <h4 className="nopass">No.of Passengers</h4>
+          <div className='nopassfield'>
+          <input type="text" placeholder="No.of Passengers" className="nopass"/>
+          </div>
+          <button type="submit" className="submit">Submit</button>
+
+
+          </div>
+
       </div>
     )
 }
+
+
+
+export default BookTicket;
