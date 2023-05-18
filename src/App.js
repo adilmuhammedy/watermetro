6230d68e27fb9bc9bf3651f51cdf03e377daddf3
ADIL MUHAMMED Y <adilmuhammedy@gmail.com>
2023-05-18 17:20:32 +0530

    new updates by adil

--- src/App.js
+++ src/App.js
@@ -1,13 +1,15 @@
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import Home from './Homepage';
 import BookTickets from './bookticket';
+import Terminals from './terminals';
 
 function App() {
   return (
     <Router>
       <Switch>
         <Route exact path="/" component={Home} />
-        <Route path="/book-tickets" component={BookTickets} />
+        <Route path="/bookticket" component={BookTickets} />
+        <Route path="/terminals" component={Terminals} />
       </Switch>
     </Router>
   );
--- src/Homepage.js
+++ src/Homepage.js
@@ -1,14 +1,28 @@
-import React from 'react'
+import React from 'react';
 import './Homepage.css';
-import { Link } from 'react-router-dom';
+import { useHistory } from 'react-router-dom';
 
 function Home() {
+  const history = useHistory();
+
+  const handleHomeClick = () => {
+    history.push('/');
+  }
+
+  const handleBookTicketsClick = () => {
+    history.push('/bookticket');
+  }
+
+  const handleTerminalsClick = () => {
+    history.push('/terminals');
+  }
+
   return (
     <div className="Home">
       <header className="home-header">
-        <h4 className="home">HOME</h4>
-        <Link to="/book-tickets" className="booktickets">BOOK TICKETS</Link>
-        <h4 className="terminals">TERMINALS</h4>
+        <h4 className="home" onClick={handleHomeClick}>HOME</h4>
+        <h4 className="booktickets" onClick={handleBookTicketsClick}>BOOK TICKETS</h4>
+        <h4 className="terminals" onClick={handleTerminalsClick}>TERMINALS</h4>
         <h4 className="faredetails">FARE DETAILS</h4>
         <h4 className="login">LOGIN</h4>
       </header>
