67280b091c64e52aebae6bfc5771298677e34ee1
Seethal S <seethals001@gmail.com>
2023-05-19 10:13:16 +0530

    seethal commits

---
+++ src/fare.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import './fare.css';
+
+function faredetails(){
+    return (
+        <div className="Home">
+        <header className="home-header">
+          <h4 className="home">HOME</h4>
+          <h4 className="booktickets">BOOK TICKETS</h4>
+          <h4 className="terminals">TERMINALS</h4>
+          <h4 className="faredetails">FARE DETAILS</h4>
+          <h4 className="login">LOGIN</h4>
+        </header>
+        <div className="rectangle">
+        </div>
+
+        <h2 className="fare">FARE DETAILS</h2>
+        </div>
+        )
+}
--- src/register.js
+++ src/register.js
@@ -1,7 +1,6 @@
 import React, { useState } from 'react';
 import { useHistory } from 'react-router-dom';
 import {Link} from 'react-router-dom';
-//import './register.css'; 
 import './register.css';
 
 export const Register = () => {
